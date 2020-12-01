d165cc5f740e6ba3bc4d6f96747ad049ad3dc3e0
Oleks Chemerys <oleks@quick.cz>
2020-11-30 19:43:15 -0700

    navigation guards

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import routes from './routes';
-// import store from '../store';
+import store from '../store';
 // import auth from '../../config/firebase-auth.conf';
 
 Vue.use(Router);
@@ -13,34 +13,30 @@ const router = new Router({
 });
 
 // --- route guards
-// router.beforeEach((to, from, next) => {
-//   console.log('router - auth.user', store.state);
-//   console.log('router.beforeEach::to , from', to, from);
-//   next();
-// //   if (to.matched.some((record) => record.meta.requiresAuth)) {
-// //     if (!self.$store.getters.isLoggedIn) {
-// //       next({
-// //         path: '/',
-// //         params: { nextUrl: to.fullPath },
-// //       });
-// //     } else if (to.matched.some((record) => record.meta.isAdmin)) {
-// //       if (self.$store.getters.isLoggedUserAdmin) {
-// //         next();
-// //       } else {
-// //         next({ name: 'profile' });
-// //       }
-// //     } else {
-// //       next();
-// //     }
-// //   } else if (to.matched.some((record) => record.meta.guest)) {
-// //     if (!self.$store.getters.isLoggedIn) {
-// //       next();
-// //     } else {
-// //       next({ name: 'profile' });
-// //     }
-// //   } else {
-// //     next();
-// //   }
-// });
+router.beforeEach((to, from, next) => {
+  // console.log('store state', store.state);
+  // console.log('store getters isLoggedIn', store.getters.isLoggedIn);
+  // console.log('store getters isLoggedUserAdmin', store.getters.isLoggedUserAdmin);
+
+  // console.log('localeStorage',
+  // JSON.parse(localStorage.getItem('Auth:User:<AuthKey>:default')) || {});
+
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (!store.getters.isLoggedIn) {
+      next({ name: 'landing' });
+    }
+    if (to.matched.some((record) => record.meta.isAdmin)) {
+      if (!store.getters.isLoggedUserAdmin && to.Name === 'dashboard') {
+        next({ name: 'profile' });
+      } else {
+        next();
+      }
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
 
 export default router;
